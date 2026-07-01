c5db41dd385166327e6cd5d27d84f4966a90e582
Samuel Stark <samuel.winslow.stark@gmail.com>
2026-07-01 19:09:56 +0100

    Many many units

--- turnip_api_search/src/conversions/mod.rs
+++ turnip_api_search/src/conversions/mod.rs
@@ -96,12 +96,15 @@ pub enum Value {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum NumUnit {
     Length(LengthUnit),
+    Temp(TempUnit),
+    Time(TimeUnit),
     Currency(ArrayString<3>),
+    // TODO area, volume
     // ...
 }
 
-trait NumUnitGroup {
-    // for (x, y) base_unit = (val * x) + y
+trait NumUnitGroup: std::fmt::Debug + Clone + Copy + PartialEq + Eq + std::hash::Hash {
+    /// Express the conversion of units in 'self' to units in 'base' as (x, y) where `base_unit = (self * x) + y`
     fn of_base(self) -> (f64, f64);
 }
 
@@ -118,9 +121,19 @@ pub enum LengthUnit {
     Ft,
     /// Stored as feet internally but converted to feet'inches'' for display
     FtIn,
-    Mile,
-    NauticalMile,
     Yd,
+    Mile,
+    League,
+
+    NauticalMile,
+    NauticalLeague,
+    Fathom,
+
+    LightYear,
+    Au,
+    Parsec,
+
+    Furlong,
 }
 impl NumUnitGroup for LengthUnit {
     fn of_base(self) -> (f64, f64) {
@@ -132,11 +145,69 @@ impl NumUnitGroup for LengthUnit {
             LengthUnit::Um => (1E-6, 0.0),
             LengthUnit::Nm => (1E-9, 0.0),
             LengthUnit::Pm => (1E-12, 0.0),
+
             LengthUnit::In => (0.0254, 0.0),
             LengthUnit::Ft | LengthUnit::FtIn => (0.3048, 0.0),
-            LengthUnit::Mile => (1609.344, 0.0),
-            LengthUnit::NauticalMile => (1852.0, 0.0),
             LengthUnit::Yd => (0.9144, 0.0),
+            LengthUnit::Mile => (1609.344, 0.0),
+            LengthUnit::League => (1609.344 * 3.0, 0.0), // three miles
+
+            LengthUnit::NauticalMile => (1852.0, 0.0),
+            LengthUnit::NauticalLeague => (1852.0 * 3.0, 0.0), // three nautical miles
+            LengthUnit::Fathom => (1.8288, 0.0),
+
+            LengthUnit::LightYear => (9460730472580.8 * 1E3, 0.0),
+            LengthUnit::Au => (149597870700.0, 0.0),
+            LengthUnit::Parsec => (30856775814913673.0, 0.0),
+
+            LengthUnit::Furlong => (201.168, 0.0),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum TempUnit {
+    C,
+    F,
+    K,
+}
+impl NumUnitGroup for TempUnit {
+    fn of_base(self) -> (f64, f64) {
+        match self {
+            TempUnit::C => (1.0, 0.0),                    // C = C * 1 + 0
+            TempUnit::F => (5.0 / 9.0, 32.0 * 5.0 / 9.0), // F = C * 1.8 + 32, C = (F - 32) / 1.8 = F * 5/9 - (32*5)/9 =
+            TempUnit::K => (1.0, 273.15),                 // K = C * 1 - 273.15, C = K * 1 + 273.15
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum TimeUnit {
+    S,
+    Ms,
+    Us,
+    Ns,
+    Ps,
+    Min,
+    Hour,
+    Day,
+    Week,
+    // Month, // Can't convert reliably
+    Year,
+}
+impl NumUnitGroup for TimeUnit {
+    fn of_base(self) -> (f64, f64) {
+        match self {
+            TimeUnit::S => (1.0, 0.0),
+            TimeUnit::Ms => (1E-3, 0.0),
+            TimeUnit::Us => (1E-6, 0.0),
+            TimeUnit::Ns => (1E-9, 0.0),
+            TimeUnit::Ps => (1E-12, 0.0),
+            TimeUnit::Min => (60.0, 0.0),
+            TimeUnit::Hour => (3600.0, 0.0),
+            TimeUnit::Day => (3600.0 * 24.0, 0.0),
+            TimeUnit::Week => (3600.0 * 24.0 * 7.0, 0.0),
+            TimeUnit::Year => (3600.0 * 24.0 * 365.25, 0.0),
         }
     }
 }
@@ -145,6 +216,7 @@ type NumberUnits = ArrayVec<NumUnit, 15>;
 
 type NumberUnitStr = SmolStr;
 
+#[derive(Debug)]
 enum InternalConversion {
     Number {
         input_unit: NumUnit,
@@ -160,6 +232,7 @@ enum InternalConversion {
     },
 }
 
+#[derive(Debug)]
 pub enum Conversion {
     Number(String),
     DateTime(String),
@@ -169,7 +242,7 @@ macro_rules! basic_conv {
     ($i:expr, $o:expr, $val:expr) => {{
         let (ia, ib) = $i.of_base();
         let (oa, ob) = $o.of_base();
-        $val * (ia / oa) + ((ib - ob) / oa)
+        $val * (ia / oa) + ((ob - ib) / oa)
     }};
 }
 
@@ -349,7 +422,28 @@ const CURRENCIES: [(&str, &str); 173] = [
     ("ZWG", "Zimbabwean ZiG"),
     ("ZWL", "Zimbabwean Dollar"),
 ];
-const CURRENCY_PREFIXES: [(char, &str); 3] = [('$', "USD"), ('£', "GBP"), ('€', "EUR")];
+const CURRENCY_PREFIXES: [(char, &str); 16] = [
+    ('$', "USD"),
+    ('£', "GBP"),
+    ('€', "EUR"),
+    ('¥', "JPY"),
+    ('￥', "JPY"),
+    // ('₩', "KPW"), // irrelevant/difficult to measure
+    ('₩', "KRW"),
+    // ('￦', "KPW"), // irrelevant/difficult to measure
+    ('￦', "KRW"),
+    ('৳', "BDT"),
+    ('⃀', "KGS"),
+    ('₪', "ILS"),
+    // ('₨', ""), // there are a lot of rupees, like a lot of dollars/pesos which use $, so don't put in a canonical one
+    ('₹', "INR"),
+    ('₽', "RUB"),
+    ('៛', "KHR"),
+    ('⃁', "SAR"),
+    ('﷼', "IRR"),
+    ('₱', "PHP"),
+    // Got bored, didn't do the rest
+];
 
 pub struct ConversionCtx {
     str_to_num_unit: FnvHashMap<NumberUnitStr, NumberUnits>,
@@ -371,6 +465,8 @@ impl ConversionCtx {
 
         use LengthUnit::*;
         use NumUnit::*;
+        use TempUnit::*;
+        use TimeUnit::*;
         let mut basic_unit_suffix = |unit, name: &str, suffix: &str, parsers: &[&str]| {
             num_unit_to_name
                 .entry(unit)
@@ -459,14 +555,153 @@ impl ConversionCtx {
             "ft/in",
             &["foot", "feet", "ft"],
         );
+        basic_unit_suffix(Length(Yd), "yds", " yards", &["yard", "yards", "yd", "yds"]);
         basic_unit_suffix(Length(Mile), "miles", " miles", &["mile", "miles"]);
+        basic_unit_suffix(
+            Length(League),
+            "leagues",
+            " leagues",
+            &["league", "leagues"],
+        );
         basic_unit_suffix(
             Length(NauticalMile),
             "nautical miles",
             " nautical miles",
-            &["nautical mile", "nautical miles"],
+            // If the user specifies the base, they may want this
+            &[
+                "mile",
+                "miles",
+                "nautical mile",
+                "nautical miles",
+                "nmi",
+                "nmis",
+            ],
         );
-        basic_unit_suffix(Length(Yd), "yds", " yards", &["yard", "yards", "yd", "yds"]);
+        basic_unit_suffix(
+            Length(NauticalLeague),
+            "nautical leagues",
+            " nautical leagues",
+            // If the user specifies the base, they may want this
+            &["league", "leagues", "nautical league", "nautical leagues"],
+        );
+        basic_unit_suffix(
+            Length(Fathom),
+            "fathoms",
+            " fathoms",
+            &["fathom", "fathoms"],
+        );
+
+        basic_unit_suffix(
+            Length(LightYear),
+            "light years",
+            "ly",
+            &[
+                "ly",
+                "lys",
+                "lyr",
+                "lyrs",
+                "light year",
+                "light-year",
+                "lightyear",
+                "light years",
+                "light-years",
+                "lightyears",
+            ],
+        );
+        basic_unit_suffix(
+            Length(Au),
+            "astronomical units",
+            "au",
+            &["au", "aus", "astronomical unit", "astronomical units"],
+        );
+        basic_unit_suffix(
+            Length(Parsec),
+            "parsecs",
+            "pc",
+            &["pc", "pcs", "parsec", "parsecs"],
+        );
+        basic_unit_suffix(
+            Length(Furlong),
+            "furlongs",
+            "fur",
+            &["fur", "furs", "furlong", "furlongs"],
+        );
+
+        basic_unit_suffix(
+            Temp(C),
+            "°C", // degree sign prefix renders better than dedicated degrees-C
+            "°C",
+            &[
+                "celsius", //
+                "c",       //
+                "°c",      // degree sign prefix
+                "˚c",      // 'ring above' prefix, for permissivity
+                "∘c",      // 'ring operator' prefix, for permissivity
+                "℃",       // dedicated "degrees-C" character
+            ],
+        );
+        basic_unit_suffix(
+            Temp(F),
+            "°F", // degree sign prefix renders better than dedicated degrees-F
+            "°F",
+            &[
+                "fahrenheit", //
+                "f",          //
+                "°f",         // degree sign prefix
+                "˚f",         // 'ring above' prefix, for permissivity
+                "∘f",         // 'ring operator' prefix, for permissivity
+                "℉",          // dedicated "degrees-F" character
+            ],
+        );
+        basic_unit_suffix(
+            Temp(K),
+            "kelvin",
+            "K",
+            &[
+                "kelvin", //
+                "k",      //
+                "°k",     // degree sign prefix
+                "˚k",     // 'ring above' prefix, for permissivity
+                "∘k",     // 'ring operator' prefix, for permissivity
+                          // there is no "degrees-K" character
+            ],
+        );
+
+        basic_unit_suffix(Time(S), "seconds", "s", &["s", "second", "seconds"]);
+        basic_unit_suffix(
+            Time(Ms),
+            "milliseconds",
+            "ms",
+            &["ms", "millisecond", "milliseconds"],
+        );
+        basic_unit_suffix(
+            Time(Us),
+            "microseconds",
+            "μs",
+            &["μs", "microsecond", "microseconds"],
+        );
+        basic_unit_suffix(
+            Time(Ns),
+            "nanoseconds",
+            "ns",
+            &["ns", "nanosecond", "nanoseconds"],
+        );
+        basic_unit_suffix(
+            Time(Ps),
+            "picoseconds",
+            "ps",
+            &["ps", "picosecond", "picoseconds"],
+        );
+        basic_unit_suffix(
+            Time(Min),
+            "minutes",
+            "mins",
+            &["min", "mins", "minute", "minutes"],
+        );
+        basic_unit_suffix(Time(Hour), "hours", "hrs", &["hr", "hrs", "hour", "hours"]);
+        basic_unit_suffix(Time(Day), "days", " days", &["dy", "dys", "day", "days"]);
+        basic_unit_suffix(Time(Week), " weeks", "wks", &["wk", "wks", "week", "weeks"]);
+        basic_unit_suffix(Time(Year), " years", "yrs", &["yr", "yrs", "year", "years"]);
 
         for (currency_str, _currency_written) in CURRENCIES {
             let unit = Currency(ArrayString::from(currency_str).unwrap());
@@ -531,6 +766,8 @@ impl ConversionCtx {
         use NumUnit::*;
         let o_val = match (input_unit, output_unit) {
             (Length(i), Length(o)) => basic_conv!(i, o, val),
+            (Temp(i), Temp(o)) => basic_conv!(i, o, val),
+            (Time(i), Time(o)) => basic_conv!(i, o, val),
             (Currency(i), Currency(o)) => {
                 let i_to_usd = self.currencies_to_usd.get(&i)?;
                 let o_to_usd = self.currencies_to_usd.get(&o)?;
@@ -575,7 +812,7 @@ impl ConversionCtx {
                     ),
                     _ => {
                         // TODO format with scientific 4sig-figs if <0.001 or >1E10, to 4sig-figs if <10_000, and rounded to integer otherwise
-                        // This is a personal heuristic.
+                        // This is a personal heuristic. It's non-exact for currency conversion but you can click through for that. I would round to 2d.p. for currency specifically but there are some currencies (e.g. yen) where that does not work
                         // Currently approximated with numfmt: <10_000 use a formatter with 4sf, which automatically kicks in scientific if <0.0001. (close enough for me).
                         // >=10_000, <1E11 (numfmt scientific starting point) = 0dp precision
                         // >= 1E11 = 4sf again.
@@ -621,8 +858,10 @@ impl ConversionCtx {
     pub fn parse_and_convert(&self, query: &str) -> Option<Vec<Conversion>> {
         let (input_val, input_unit, output_unit) = parser::parse_conversion(query)?;
 
-        let input_unit = &input_unit.to_ascii_lowercase_smolstr();
-        let output_unit = &output_unit.to_ascii_lowercase_smolstr();
+        let input_unit = &input_unit.trim().to_ascii_lowercase_smolstr();
+        let output_unit = &output_unit.trim().to_ascii_lowercase_smolstr();
+
+        // dbg!(query, input_unit, output_unit);
 
         match input_val {
             Value::Number(input_val) => {
